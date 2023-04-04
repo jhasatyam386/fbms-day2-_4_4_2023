b70fa46b2102f38d36ef167fa9d5ea46a01c2e24
satyam jha <jhasatyam386@gamil.com>
2023-04-04 12:00:01 +0530

    Initial Commit

---
+++ fbms day2 _4_4_2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fbms day2 _4_4_2023
+//
+//  Created by R&W on 04/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
